4cd13a3fc5aa95661b89c3b7ceb40891c726fe06
Jonny Li <jonny_li@live.ca>
2023-05-23 19:35:19 -0400

    QoL/formatting

--- docs_assistant/bot/cli.py
+++ docs_assistant/bot/cli.py
@@ -1,17 +1,6 @@
 import os
 import argparse
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
 from ..chain import get_retrieval_qa_chain, ask
-
-def retrieval_qa_chain(db_dir: str) -> RetrievalQA:
-    embeddings = OpenAIEmbeddings()
-    db = Chroma(persist_directory=db_dir, embedding_function=embeddings)
-    lm = OpenAI()
-    qa = RetrievalQA.from_chain_type(llm=lm, chain_type='stuff', retriever=db.as_retriever(), return_source_documents=True)
-    return qa
 
 def main():
     parser = argparse.ArgumentParser()
--- docs_assistant/loaders/confluence.py
+++ docs_assistant/loaders/confluence.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from langchain.document_loaders import ConfluenceLoader
 from langchain.text_splitter import CharacterTextSplitter
@@ -5,21 +6,26 @@ from ..chain import embed_docs
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('db_dir')
-    parser.add_argument('url')
-    parser.add_argument('space_keys', nargs='+')
+    parser.add_argument('--db-dir')
+    parser.add_argument('--url')
+    parser.add_argument('--space-keys', nargs='+')
     parser.add_argument('--username')
     parser.add_argument('--api-key')
     args = parser.parse_args()
 
-    loader = ConfluenceLoader(url=args.url, username=args.username, api_key=args.api_key)
+    loader = ConfluenceLoader(
+        url=args.url or os.environ['CONFLUENCE_URL'],
+        username=args.username or os.environ.get('CONFLUENCE_USERNAME'),
+        api_key=args.api_key or os.environ.get('CONFLUENCE_API_KEY')
+    )
     documents = []
-    for space_key in args.space_keys:
+    for space_key in args.space_keys or os.environ['CONFLUENCE_SPACE_KEYS'].split(','):
+        print('loading docs from space:', space_key)
         documents += loader.load(space_key=space_key)
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     documents = text_splitter.split_documents(documents)
 
-    embed_docs(args.db_dir)
+    embed_docs(args.db_dir or os.environ['CHROMA_DB_DIR'])
 
 if __name__ == '__main__':
     main()
