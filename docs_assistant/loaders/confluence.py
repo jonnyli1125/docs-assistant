9cf9bb0be45cd72ce284f466403be17d52b3013d
Jonny Li <jonny_li@live.ca>
2023-05-23 21:36:36 -0400

    Use CQL

--- docs_assistant/loaders/confluence.py
+++ docs_assistant/loaders/confluence.py
@@ -6,9 +6,9 @@ from ..chain import embed_docs
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('cql')
     parser.add_argument('--db-dir')
     parser.add_argument('--url')
-    parser.add_argument('--space-keys', nargs='+')
     parser.add_argument('--username')
     parser.add_argument('--api-key')
     args = parser.parse_args()
@@ -18,10 +18,7 @@ def main():
         username=args.username or os.environ.get('CONFLUENCE_USERNAME'),
         api_key=args.api_key or os.environ.get('CONFLUENCE_API_KEY')
     )
-    documents = []
-    for space_key in args.space_keys or os.environ['CONFLUENCE_SPACE_KEYS'].split(','):
-        print('loading docs from space:', space_key)
-        documents += loader.load(space_key=space_key)
+    documents = loader.load(cql=args.cql)
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     documents = text_splitter.split_documents(documents)
 
